396e2e3b72221d8b0af1d7d8900509fe32eac73d
Joe <jpollis.slp@gmail.com>
2018-03-15 15:07:55 -0400

    getting site on heroku

--- public/profile.js
+++ public/profile.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
     $('.parallax').parallax();
+    $("#proj").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".projects").offset().top},
+        'slow');
+});
   });
