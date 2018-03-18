3c26ee5def9fcd42cf1f22e9610316d9c042cc59
Joe <jpollis.slp@gmail.com>
2018-03-18 00:31:54 -0400

    projects link on sidenav

--- public/profile.js
+++ public/profile.js
@@ -6,13 +6,18 @@ $(document).ready(function(){
       scrollTop: $(".projects").offset().top},
       'slow');
     });
+    $("#proj2").click(function() {
+      $('html,body').animate({
+        scrollTop: $(".projects").offset().top},
+        'slow');
+      });
     // sidenav activate
     // $(".button-collapse").sideNav();
 
     $('.button-collapse').sideNav({
       menuWidth: 300, // Default is 300
       edge: 'left', // Choose the horizontal origin
-      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      closeOnClick: false, // Closes side-nav on <a> clicks, useful for Angular/Meteor
       draggable: true // Choose whether you can drag to open on touch screens
     }
   );
